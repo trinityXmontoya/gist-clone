0f3f3cf104332a3621b7809a3c2dd5acf99cffdf
trinityXmontoya <trinityXmontoya@gmail.com>
2019-08-08 23:56:00 -0700

    add datomic lib

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/trinityXmontoya/gist-clone"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.datomic/client-pro "0.8.28"]]
   :main ^:skip-aot gist-clone.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
