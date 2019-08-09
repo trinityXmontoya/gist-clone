6f6cb94a63ca40b1505a1d60e0471c670e808a68
trinityXmontoya <trinityXmontoya@gmail.com>
2019-08-08 21:22:18 -0700

    more info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gist-clone "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clone of gist.github.com"
+  :url "https://github.com/trinityXmontoya/gist-clone"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
